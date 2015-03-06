cd1ede80f272a3a8ba7d3072b1a13d958d86576d
Chad Burt <chad@underbluewaters.net>
2015-03-06 07:57:13 -1000

    Initial Commit

---
+++ eFins-iOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  eFins-iOS
+//
+//  Created by CHAD BURT on 3/6/15.
+//  Copyright (c) 2015 McClintock Lab. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
